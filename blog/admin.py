4aa115a4ca2b3acf82fd1a9b899801a8a3e0a983
Burak <burakbasturkk@icloud.com>
2023-08-17 15:51:02 +0300

    yorum için model oluşturuldu, admin.py üzerine eklendi

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Blog,Category
+from .models import Blog,Category, Post, Comment
 from django.utils.safestring import mark_safe
 
 class BlogAdmin(admin.ModelAdmin):
@@ -9,7 +9,15 @@ class BlogAdmin(admin.ModelAdmin):
     readonly_fields = ("slug",)
     list_filter = ("is_active","is_home","categories",)
 
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
 
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
 
 
 admin.site.register(Blog, BlogAdmin)
--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.text import slugify
 from ckeditor.fields import RichTextField
 from autoslug import AutoSlugField
+from django.contrib.auth.models import User
 
 
 class Category (models.Model):
@@ -11,7 +12,6 @@ class Category (models.Model):
     adres = models.CharField (max_length=150, default="adres", blank=True)
     slug = models.SlugField(null=False, blank=True,unique=True, db_index=True,editable=False)
     #slug = AutoSlugField(populate_from ='name', always_update = True,)
-   
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super().save(*args, **kwargs)
@@ -39,4 +39,35 @@ class Blog (models.Model):
         super().save(*args, **kwargs)
 
 
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now= True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
 
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
